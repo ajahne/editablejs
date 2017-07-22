b67d981614af738653b464c1f96005631f7476ff
ajahne <ajahne@gmail.com>
2017-07-22 16:48:57 -0400

    code cleanup

--- src/app.js
+++ src/app.js
@@ -1,9 +1,5 @@
-// document.designMode = "on";
 var id = "hello-world";
 var el = document.getElementById(id);
-// el.document.execCommand("bold", false, "");
-// document.execCommand("bold", false, null);
-// el.style.color="red";
 
 function doBold() {
   document.execCommand("bold", false, null);
