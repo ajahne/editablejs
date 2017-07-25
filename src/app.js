3852c910f45cb59a6eab42c24f9c9d940562d35e
ajahne <ajahne@gmail.com>
2017-07-24 21:25:29 -0400

    controls dynamically show when text is highlighted from a double click

--- src/app.js
+++ src/app.js
@@ -1,6 +1,27 @@
 var id = "hello-world";
 var el = document.getElementById(id);
+var controls = document.getElementById('controls');
+
+
+function init() {
+  hideControls();
+}
 
 function doBold() {
   document.execCommand("bold", false, null);
+  hideControls();
 }
+
+function onDoubleClick() {
+  showControls();
+}
+
+function showControls() {
+  controls.style.display = "block";
+}
+
+function hideControls() {
+  controls.style.display = "none";
+}
+
+init();
