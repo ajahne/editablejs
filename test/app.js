6e7c83a2255d75c874a618c4cd29f4e3bd3f06dd
ajahne <ajahne@gmail.com>
2017-07-29 01:29:01 -0400

    added initial tests

---
+++ test/app.js
@@ -0,0 +1,15 @@
+// const assert = require('assert');
+let assert = chai.assert;
+const HELLO_WORLD = 'hello world';
+
+describe('rich text controls', function() {
+  let controls = document.getElementById('controls');
+  it('shoudl show controls', function() {
+    app.showControls();
+    assert.equal(controls.style.display, 'block');
+  });
+  it('should hide controls', function() {
+    app.hideControls();
+    assert.equal(controls.style.display, 'none');
+  });
+});
