f1a5bb2305c0f8fce17035f68ea974d2635baa7c
ajahne <ajahne@gmail.com>
2017-08-21 23:29:09 -0400

    updated tests

--- test/app.js
+++ test/app.js
@@ -10,9 +10,14 @@ describe('rich text controls', function() {
     app.hideControls();
     assert.equal(controls.style.display, 'none');
   });
-  it ('should move to the x position', function() {
-    let x = 500;
+  it ('should move 100px from the left', function() {
+    let x = 100;
     app.setPosition(x, 0);
     assert.equal(controls.style.left, x + 'px');
+  });
+  it ('should move 100px from the top', function() {
+    let y = 100;
+    app.setPosition(0, y);
+    assert.equal(controls.style.top, y + 'px');
   });
 });
