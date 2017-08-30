0989c974c54133ae58430abf98b6596529f1c30b
ajahne <ajahne@gmail.com>
2017-08-29 22:47:15 -0400

    updated to better handle user interaction

--- src/selection.js
+++ src/selection.js
@@ -1,20 +1,40 @@
 (() => {
   let selection;
-  let selectionText;
+  let currentSelectionText = "";
+  let previousSelectionText = "";
 
-  document.onselectstart = () => {
-    console.log('Selection Start');
+  let setSelection = () => {
+    selection = document.getSelection();
+  }
+
+  let setCurrentSelectionText = (value) => {
+    currentSelectionText = value;
+  }
+
+  let setSelectionAndCurrentSelectionText = () => {
+    setSelection(document.getSelection());
+    setCurrentSelectionText(selection.toString());
   }
 
   document.onselectionchange = () => {
-    selection = document.getSelection();
-    selectionText = selection.toString();
-    console.log('On Selection Change %o', selectionText);
-    if (selectionText.length === 0) {
-      app.hideControls();
-    }
-    else {
+    setSelectionAndCurrentSelectionText();
+  }
+
+  document.onmousedown = () => {
+    //clear current selection
+    setCurrentSelectionText("");
+  }
+
+  document.onmouseup = () => {
+    //only show controls if the current selection
+    //is diff from previous selection
+    if (currentSelectionText.length > 0 &&
+        currentSelectionText !== previousSelectionText) {
       app.showControls();
     }
+    else {
+      app.hideControls();
+    }
+    previousSelectionText = currentSelectionText;
   }
 })();
