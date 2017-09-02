34f454a9c3877ec1fd2141ed2b0778faa75d3401
ajahne <ajahne@gmail.com>
2017-09-01 22:00:42 -0400

    added initial selectionchange simulation

--- test/selection.js
+++ test/selection.js
@@ -1,5 +1,27 @@
-describe('selection', function() {
-  xit ('should clear the selection', function() {
+/*
+* What should the selection do?
+* show controls when a new set of text has been selected
+* hide controls when no selection
+*/
 
+let simulateSelectionChange = () => {
+  let event = new Event('selectionchange', {
+    name:'foo',
+      toString:function() {
+        return 'selected text';
+    }
+  });
+  document.dispatchEvent(event);
+}
+
+describe('selection', function() {
+  it ('should show the controls', function() {
+    let onSelectionChange = (e) => {
+      console.log('selection test');
+      console.log(e);
+    };
+    document.addEventListener('selectionchange',onSelectionChange);
+    simulateSelectionChange();
+    document.removeEventListener('selectionchange',onSelectionChange);
   });
 });
