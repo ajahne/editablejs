6b21d5d947e4d1c6f9bf365ee5c1252bea980cfa
ajahne <ajahne@gmail.com>
2017-09-02 19:16:24 -0400

    added sandbox to clean up stubs after tests

--- test/selection.js
+++ test/selection.js
@@ -4,6 +4,9 @@
 * hide controls when no selection
 */
 (() => {
+  // let assert = chai.assert;
+  let sandbox = sinon.createSandbox();
+
   let simulateSelectionChange = () => {
     let event = new Event('selectionchange');
     document.dispatchEvent(event);
@@ -19,19 +22,27 @@
     document.dispatchEvent(event);
   };
 
-  let stub = sinon.stub(document, 'getSelection');
-  stub.returns({
-    toString:function() {
-      return 'selected text';
-    }
-  });
-
   describe('selection', function() {
+    beforeEach(function () {
+      let stub = sandbox.stub(document, 'getSelection');
+      stub.returns({
+        toString:function() {
+          return 'selected text';
+        }
+      });
+    });
+
+    afterEach(function () {
+      // completely restore all fakes created through the sandbox
+      sandbox.restore();
+    });
+
     it ('should show the controls', function() {
       simulateSelectionChange();
       simulateMouseUp();
       assert.equal(controls.isVisible(), true);
     });
+
     it ('should hide the controls', function() {
       simulateSelectionChange();
       simulateMouseUp();
