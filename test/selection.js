62899bb70c4737c895d3b438f628e8270bfc6d76
ajahne <ajahne@gmail.com>
2017-09-01 23:04:29 -0400

    wrapped in an IFFE, code cleanup

--- test/selection.js
+++ test/selection.js
@@ -3,40 +3,41 @@
 * show controls when a new set of text has been selected
 * hide controls when no selection
 */
-
-let simulateSelectionChange = () => {
-  let event = new Event('selectionchange');
-  document.dispatchEvent(event);
-}
-
-let simulationMouseUp = () => {
-  let event = new MouseEvent('mouseup');
-  document.dispatchEvent(event);
-}
-
-let simulationMouseDown = () => {
-  let event = new MouseEvent('mousedown');
-  document.dispatchEvent(event);
-}
-
-let stub = sinon.stub(document, 'getSelection');
-stub.returns({
-  toString:function() {
-    return 'selected text';
+(() => {
+  let simulateSelectionChange = () => {
+    let event = new Event('selectionchange');
+    document.dispatchEvent(event);
   }
-});
 
-describe('selection', function() {
-  let controls = document.getElementById('controls');
-  it ('should show the controls', function() {
-    simulateSelectionChange();
-    simulationMouseUp();
-    assert.equal(controls.style.display, "block");
+  let simulateMouseUp = () => {
+    let event = new MouseEvent('mouseup');
+    document.dispatchEvent(event);
+  }
+
+  let simulateMouseDown = () => {
+    let event = new MouseEvent('mousedown');
+    document.dispatchEvent(event);
+  }
+
+  let stub = sinon.stub(document, 'getSelection');
+  stub.returns({
+    toString:function() {
+      return 'selected text';
+    }
   });
-  it ('should hide the controls', function() {
-    simulateSelectionChange();
-    simulationMouseUp();
-    simulationMouseDown();
-    assert.equal(controls.style.display, "none");
+
+  describe('selection', function() {
+    let controls = document.getElementById('controls');
+    it ('should show the controls', function() {
+      simulateSelectionChange();
+      simulateMouseUp();
+      assert.equal(controls.style.display, "block");
+    });
+    it ('should hide the controls', function() {
+      simulateSelectionChange();
+      simulateMouseUp();
+      simulateMouseDown();
+      assert.equal(controls.style.display, "none");
+    });
   });
-});
+})();
