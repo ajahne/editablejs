f70162f5d536cb715aacec89cffa733745284bd0
ajahne <ajahne@gmail.com>
2017-09-03 18:35:29 -0400

    refactored common test code to utils

---
+++ test/utils.js
@@ -0,0 +1,49 @@
+const boudingClientRect = {
+  bottom: 34,
+  height: 18,
+  left: 39,
+  right: 79.140625,
+  top: 16,
+  width: 40.140625
+};
+const selectionText = 'Hello World';
+const getSelectionResult = {
+  toString:function() {
+    return selectionText;
+  },
+  getRangeAt:function() {
+    return {
+      getBoundingClientRect: function() {
+        return boudingClientRect;
+      }
+    }
+  }
+};
+
+let simulateSelectionChange = () => {
+  let event = new Event('selectionchange');
+  document.dispatchEvent(event);
+};
+
+let simulateSelectionStart = () => {
+  let event = new Event('selectionstart');
+  document.dispatchEvent(event);
+};
+
+let simulateMouseUp = (clientX = 50, pageY = 50) => {
+  let event = new MouseEvent('mouseup', {
+    clientX: clientX,
+    pageY: pageY,
+  });
+  document.dispatchEvent(event);
+}
+
+// let simulateMouseUp = () => {
+//   let event = new MouseEvent('mouseup');
+//   document.dispatchEvent(event);
+// };
+
+let simulateMouseDown = () => {
+  let event = new MouseEvent('mousedown');
+  document.dispatchEvent(event);
+};
