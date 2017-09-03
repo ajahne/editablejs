8dd993b352b53a9f912814291946c971c9846df9
ajahne <ajahne@gmail.com>
2017-09-03 17:37:20 -0400

    now gets props based on the range's bounding rect

--- src/selection-box.js
+++ src/selection-box.js
@@ -3,21 +3,56 @@ let selectionBox = (() => {
   let y = 0;
   let width = 0;
   let height = 0;
+  let selection;
+  let range;
+  let rangeBoundingClientRect;
+
+  /*
+   * What are we doing?
+   * grab a selection and see what the properties are
+   */
+  let setPropertiesFromSelection = value => {
+    setSelection(value);
+    setRange(selection.getRangeAt(0));
+    setBoundingRectProperties(range.getBoundingClientRect());
+  };
+
+  let setRange = value => {
+    range = value;
+    console.log(range.getBoundingClientRect());
+  };
+
+  let setSelection = value => {
+    selection = value;
+  };
 
   let getX = () => x;
   let getY = () => y;
   let getWidth = () => width;
   let getHeight = () => height;
 
-  document.addEventListener('mouseup', (e) => {
-    x = e.pageX;
-    y = e.pageY;
-  }, false);
+  /**
+  * Example
+  *
+  * bottom: 34
+  * height: 18
+  * left: 39
+  * right: 79.140625
+  * top: 16
+  * width: 40.140625
+  */
+  let setBoundingRectProperties = rect => {
+      x = rect.left;
+      y = rect.top;
+      width = rect.width;
+      height = rect.height;
+  }
 
   return {
     getX,
     getY,
     getWidth,
-    getHeight
+    getHeight,
+    setPropertiesFromSelection
   };
 })();
