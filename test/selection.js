7ec8575f0268860522a88da20fcba400750fd350
ajahne <ajahne@gmail.com>
2017-09-08 21:29:44 -0400

    corrected test names

--- test/selection.js
+++ test/selection.js
@@ -32,11 +32,11 @@
     });
 
     describe('selection-box', function() {
-      it('should return the x position of the mouse', function() {
+      it('should return the x position of the selection', function() {
         let x = selectionBox.getX();
         assert.equal(typeof x, 'number');
       });
-      it('should return the y position of the mouse', function() {
+      it('should return the y position of the selection', function() {
         let y = selectionBox.getY();
         assert.equal(typeof y, 'number');
       });
